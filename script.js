31ee582f42e42860cf7051626ae86a38dc6c252a
Patryk Baranek <cien2460045@gmail.com>
2020-10-20 20:27:02 +0200

    adding random photos to website

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
 const count = 10;
 const apiKey = 'H_mtLlXn9U-jD-ryd3kUiILdOIl2egMcV6znBq88dcw';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&random=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Helper Function to set Attributes on DOM Elements
+function setAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        //Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        setAttribute(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        //Create <img> for photo
+        const img = document.createElement('img');
+        setAttribute(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+        // Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 // Get photos from Unplash API
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
+        // console.log(photosArray);
     } catch (error) {
         // Catch Error
     }
 }
+
+window.addEventListener('scroll', () => {
+    console.log('scrolled');
+});
 
 // On Load
 
